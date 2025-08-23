3cab31ad81a33926c0190cafeb5c462cb0b55a93
Austen Mack <austenmack@gmail.com>
2025-08-23 16:36:08 -0700

    refactor(filetree): use FileIcon default import; minor style tidy

--- src/components/FileTree.tsx
+++ src/components/FileTree.tsx
@@ -3,7 +3,7 @@ import FileTreeNode from './FileTreeNode';
 
 const FileTree = ({ rootNode }: { rootNode: FileNode }) => {
   return (
-    <div className="h-screen border border-r bg-neutral-800">
+    <div className="h-full border border-r bg-neutral-800">
       <div className="mb-1 text-xl text-neutral-300">Files</div>
       <div>
         {rootNode.children?.map((child) => (
