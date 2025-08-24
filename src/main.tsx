9352c702feb5c7c3b788ed16ae6cc5ef8f23fe47
Austen Mack <austenmack@gmail.com>
2025-08-24 14:15:56 -0700

    fix(scrollbar): cross-browser styling & hover reliability (#22)
    * chore(css): add cn and apply to components
    
    * fix(scrollbar): cross-browser styling & hover reliability

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import './globals.css';
 import App from './App.tsx';
 import ActiveFileProvider from './state/ActiveFileProvider.tsx';
 
+if (navigator.userAgent.toLowerCase().includes('firefox')) {
+  document.documentElement.classList.add('is-firefox');
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ActiveFileProvider>
