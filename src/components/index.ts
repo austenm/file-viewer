7eeabbbb9afea307c8567ed1301f16bb9c95d24a
Austen Mack <austenmack@gmail.com>
2025-08-23 17:57:28 -0700

    chore(components): add barrel file exporting UI components

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as ChevronIcon } from './ChevronIcon';
+export { default as CloseIcon } from './CloseIcon';
+export { default as Editor } from './Editor';
+export { default as EmptyEditor } from './EmptyEditor';
+export { default as FileIcon } from './FileIcon';
+export { default as FileTree } from './FileTree';
+export { default as FileTreeNode } from './FileTreeNode';
+export { default as Tabs } from './Tabs';
