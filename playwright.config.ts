2dda2603279f0e1d0d03aaba4f4786aeeddf1a28
Austen Mack <austenmack@gmail.com>
2025-08-26 16:14:44 -0700

    chore(ci): run vitest coverage + playwright e2e in parallel; upload rpts (#33)
    * chore(ci): run vitest coverage + playwright e2e in parallel; upload rpts
    
    * chore(ci): speed up ci, use image

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const isCI = !!process.env.CI;
+
 export default defineConfig({
   testDir: 'tests/e2e',
+  retries: isCI ? 2 : 0,
+  timeout: 60_000,
   use: { baseURL: 'http://localhost:5173' },
   webServer: {
     command: 'pnpm dev',
     port: 5173,
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !isCI,
   },
   projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
 });
