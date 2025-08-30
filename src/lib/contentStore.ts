48d10c02e782044b9a6407aa065006ae40d50c36
Austen Mack <austenmack@gmail.com>
2025-08-29 21:02:35 -0700

    feat(utils): add test-utils, fix files reference in contentStore

--- src/lib/contentStore.ts
+++ src/lib/contentStore.ts
@@ -2,7 +2,7 @@ import files from '../data/reactTutorialFiles';
 import normalizePath from '../utils/normalizePath';
 
 const byPath = new Map<string, string>();
-for (const f of files) {
+for (const f of files.files) {
   byPath.set(normalizePath(f.path), (f.content ?? '').replace(/^\n/, ''));
 }
 
