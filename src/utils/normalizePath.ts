db81193189066115f9412e8cd581916d21e36591
Austen Mack <austenmack@gmail.com>
2025-09-14 13:42:03 -0700

    fix(normalizePath): collapse multiple slashes

--- src/utils/normalizePath.ts
+++ src/utils/normalizePath.ts
@@ -2,6 +2,7 @@ const normalizePath = (path: string) =>
   path
     .replace(/\\/g, '/') // all '\' replaced with '/'
     .replace(/^\.\/+/, '') // remove './' from start of path
+    .replace(/\/{2,}/g, '/') // collapse multiple slashes
     .replace(/\/+$/, ''); // remove trailing '/' from path
 
 export default normalizePath;
